c10cd17b25d7cdf6c5547682a0151662924b953e
denissb <spawnenius@gmail.com>
2020-02-12 00:48:29 +0100

    fix: added several languages

--- src/services/i18n.js
+++ src/services/i18n.js
@@ -3,6 +3,11 @@ import {initReactI18next} from 'react-i18next';
 import {Platform, NativeModules} from 'react-native';
 
 import en from '../translations/en';
+import de from '../translations/de';
+import dk from '../translations/dk';
+import fr from '../translations/fr';
+import lv from '../translations/lv';
+import ru from '../translations/ru';
 
 export const setup = () => {
   const languageDetector = {
@@ -29,6 +34,21 @@ export const setup = () => {
       resources: {
         en: {
           translation: en,
+        },
+        de: {
+          translation: de,
+        },
+        dk: {
+          translation: dk,
+        },
+        fr: {
+          translation: fr,
+        },
+        lv: {
+          translation: lv,
+        },
+        ru: {
+          translation: ru,
         },
       },
     });
