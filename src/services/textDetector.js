efe46a7119b7e09cf5994976b8697f7bf6e0f0e5
denissb <spawnenius@gmail.com>
2021-09-11 15:02:58 +0200

    fix: version 1.2.1

--- src/services/textDetector.js
+++ src/services/textDetector.js
@@ -2,12 +2,22 @@
  * @format
  * @flow strict-local
  */
+
 import MlkitOcr from 'react-native-mlkit-ocr';
+
+const getTextFromBlock = block =>
+  block.lines.map(({text}) => text).join('') + '\n\n';
 
 export const recogniseText = async imageURI => {
   const result = await MlkitOcr.detectFromUri(imageURI);
 
-  return result.reduce((text = '', current) => {
-    return text + current.text;
+  const resultText = result.reduce((blockResult, nextBlock) => {
+    if (typeof blockResult === 'object') {
+      return getTextFromBlock(blockResult) + getTextFromBlock(nextBlock);
+    } else {
+      return blockResult + getTextFromBlock(nextBlock);
+    }
   });
+
+  return resultText;
 };
