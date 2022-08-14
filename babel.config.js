7d9cb92d4bf744cb1cea3db250413392b4296698
denissb <spawnenius@gmail.com>
2022-08-14 19:51:08 +0200

    fix: rnx toolkit integration

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: [
+    [
+      '@rnx-kit/babel-preset-metro-react-native',
+      {disableImportExportTransform: true},
+    ],
+  ],
 };
